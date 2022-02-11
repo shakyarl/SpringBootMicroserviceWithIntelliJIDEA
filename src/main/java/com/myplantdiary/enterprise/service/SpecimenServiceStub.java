7259ba26d914b1bbd827013dc67fefd8dc506fce
Rahul Shakya <shakyarl@mail.uc.edu>
2022-02-11 17:20:23 -0500

    Add testing with mockito, unit testing for saving new specimen, update and create save and fetch methods, and use Arraylist for saving and fetching specimens

--- src/main/java/com/myplantdiary/enterprise/service/SpecimenServiceStub.java
+++ src/main/java/com/myplantdiary/enterprise/service/SpecimenServiceStub.java
@@ -1,15 +1,41 @@
 package com.myplantdiary.enterprise.service;
 
+import com.myplantdiary.enterprise.dao.ISpecimenDAO;
 import com.myplantdiary.enterprise.dto.Specimen;
-import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-@Component
+import java.util.List;
+
+@Service
 public class SpecimenServiceStub implements ISpecimenService {
+
+    @Autowired
+    private ISpecimenDAO specimenDAO;
+
+    public SpecimenServiceStub(){
+
+    }
+
+    public SpecimenServiceStub(ISpecimenDAO specimenDAO){
+        this.specimenDAO = specimenDAO;
+    }
+
     @Override
     public Specimen fetchById(int id) {
         Specimen specimen = new Specimen();
         specimen.setDescription("Eastern Redbud");
         specimen.setSpecimenId("83");
         return specimen;
+    }
+
+    @Override
+    public Specimen save(Specimen specimen) throws Exception{
+        return specimenDAO.save(specimen);
+    }
+
+    @Override
+    public List<Specimen> fetchAll() {
+        return specimenDAO.fetchAll();
     }
 }
